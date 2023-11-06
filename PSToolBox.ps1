42ddb73b59242b55677af646254d12fd63872542
jholst71 <145046295+jholst71@users.noreply.github.com>
2023-11-06 20:45:40 +0100

    Update PSToolBox.ps1
    Code Update

--- PSToolBox.ps1
+++ PSToolBox.ps1
@@ -2,9 +2,10 @@
 #
 Function Get-QueryComputers {  ### Get-QueryComputers - Get Domain Servers names 
   Param( $fQueryComputerSearch, $fQueryComputerExcludeList )
+  ## Script
     $fQueryComputers = Foreach ($fComputerSearch in $fQueryComputerSearch) {(Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' | where { $fQueryComputerExcludeList -notcontains $_.name} -ErrorAction Continue | where { ($_.name -like $fComputerSearch)} -ErrorAction Continue)};
     $fQueryComputers = $fQueryComputers | Sort Name;
-  Return $fQueryComputers;
+    Return $fQueryComputers;
 };
 Function Get-LatestReboot { ### Get-LatestReboot - Get Latest Reboot / Restart / Shutdown for logged on server
   Param(
@@ -19,7 +20,7 @@ Function Get-LatestReboot { ### Get-LatestReboot - Get Latest Reboot / Restart /
     Show-Title "Get latest Shutdown / Restart / Reboot for Local Server - Events After: $($fEventLogStartTime)";
     $fLatestBootTime = Get-WmiObject win32_operatingsystem | select csname, @{LABEL="LastBootUpTime";EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}};
     $fLatestBootEvents = Get-EventLog -LogName System -After $fEventLogStartTime | Where-Object {($_.EventID -eq 1074) -or ($_.EventID -eq 6008)};
-  # Output
+  ## Output
     # $fLatestBootEvents | Select MachineName, TimeGenerated, UserName, Message | fl; $fLatestBootEvents | Select MachineName, TimeGenerated, UserName | ft -Autosize; $fLatestBootTime;
   ## Exports
     If (($fExport -eq "Y") -or ($fExport -eq "YES")) {$fLatestBootupEvents | sort MachineName, TimeGenerated | Select MachineName, TimeGenerated, UserName, Message | Export-CSV "$($fFileName).csv" -Delimiter ';' -Encoding UTF8 -NoTypeInformation;};
@@ -80,32 +81,31 @@ Function StartSCOMMaintenanceMode {
   param( 
     $fDuration  = ("30" | %{ If($Entry = Read-Host "  Enter MaintenanceMode Duration ( Default: $_ )"){$Entry} Else {$_} }),
     $fComments = "SCOM MaintenanceMode started for $($SCOMMaintenanceModeDuration) minutes from $($env:Computername) by $($Env:USERNAME) at $(Get-Date)"
-    )
-  ## Script Begin
+  );
+  ## Script
     Import-Module "C:\Program Files\Microsoft Monitoring Agent\Agent\MaintenanceMode.dll";
     try { Start-SCOMAgentMaintenanceMode -Reason "PlannedOther" -Duration $fDuration -Comment $fComments -Force Y;
       } catch { Start-SCOMAgentMaintenanceMode -Reason "PlannedOther" -Duration $fDuration -Comment $fComments;}
     Write-Host "Request: Start SCOM Maintenance Mode for $($fDuration) minutes";
-    #Stop-Service -Name "HealthService" -force; Get-Service -Name "HealthService";
-    #Start-Service -Name "HealthService"; Get-Service -Name "HealthService";
-    };
+};
 Function Show-Title {
-    param ( [string]$Title );
+  param ( [string]$Title );
     $host.UI.RawUI.WindowTitle = $Title;
 };
 Function Show-Menu {
-    param (
-      [string]$Title = "Progressive Toolbox"
-    );
-    Show-Title $Title;
-    Clear-Host;
-    Write-Host "`n  ================ $Title ================`n";
-    #  Write-Host "  1: Press '1' for Start SCOM MaintenanceMode for Local Server.";
-    Write-Host "  5: Press '5' for Get-LatestReboot for Local Server.";
-    Write-Host "  6: Press '6' for Get-LatestReboot for Domain Servers.";
-    Write-Host "  9: Press '9' for this option.";
-    Write-Host "  I: Press 'I' for Toolbox Information.";
-    Write-Host "  Q: Press 'Q' to quit.";
+  param (
+    [string]$Title = "Progressive Toolbox"
+  );
+  Show-Title $Title;
+  Clear-Host;
+  Write-Host "`n  ================ $Title ================`n";
+  #Write-Host "  1: Press '1' for Start SCOM MaintenanceMode for Local Server.";
+  #Write-Host "  2: Press '2' for Start SCOM MaintenanceMode for Local Server (Script).";
+  Write-Host "  5: Press '5' for Get-LatestReboot for Local Server.";
+  Write-Host "  6: Press '6' for Get-LatestReboot for Domain Servers.";
+  Write-Host "  9: Press '9' for this option.";
+  Write-Host "  I: Press 'I' for Toolbox Information.";
+  Write-Host "  Q: Press 'Q' to quit.";
 };
 Function ToolboxMenu {
   do {
@@ -113,31 +113,31 @@ Function ToolboxMenu {
     $selection = Read-Host "`n  Please make a selection"
     switch ($selection){
       "1" { "`n`n  You selected: Start SCOM MaintenanceMode for Local Server`n"
-          StartSCOMMaintenanceMode;
+		StartSCOMMaintenanceMode;
         Sleep 10;
-        };
+      };
       "2" { "`n`n  You selected: Start SCOM MaintenanceMode for Local Server`n"
         $GitHubRawLink = "https://raw.githubusercontent.com/jholst71/public/main/Start_SCOM_MaintenanceMode.ps1"; IEX ((New-Object System.Net.WebClient).DownloadString($GitHubRawLink));
-        };
+      };
       "5" { "`n`n  You selected: Get-LatestReboot for Local Server`n"
         $Result = Get-LatestReboot;
         $Result.LatestBootEventsExtended | FL; $result.LatestBootEvents | FT -Autosize; $result.LatestBootTime | FT  -Autosize;
         Pause;
-        };
+      };
       "6" { "`n`n  You selected: Get-LatestReboot for Domain Servers`n"
         $Result = Get-LatestRebootDomain;
         $Result.LatestBootEvents | FT -Autosize;
         Pause;
-        };
-      "9" { "`n`n  You selected: option #3`n"
-      Sleep 10;
       };
-      "I" { "`n`n  You selected: Information option `n"
-        "  Information will be updated later"
+      "9" { "`n`n  You selected: option #3`n"
         Sleep 10;
       };
-    };
+      "I" { "`n`n  You selected: Information option `n"
+	  "  Information will be updated later"
+        Sleep 10;
+	  };
+    }; # End Switch
     #Pause;
- } until (($selection -eq "q") -or ($selection -eq "0"));
+  } until (($selection -eq "q") -or ($selection -eq "0"));
 };
 ToolboxMenu;
